e948b3df92840dfb08868abf0f3ca0efe039ab4d
saimahesh19 <117577374+saimahesh19@users.noreply.github.com>
2024-05-11 11:03:22 +0530

    Create 735. Asteroid Collision.py

---
+++ python_codes/735. Asteroid Collision.py
@@ -0,0 +1,14 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for asteroid in asteroids:
+            while stack and asteroid < 0 and stack[-1] > 0:
+                if abs(asteroid) > stack[-1]:
+                    stack.pop()
+                    continue
+                elif abs(asteroid) == stack[-1]:
+                    stack.pop()
+                break
+            else:
+                stack.append(asteroid)
+        return stack
