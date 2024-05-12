858c33e707be794ed9fd7c1ed51fa14618644be7
saimahesh19 <117577374+saimahesh19@users.noreply.github.com>
2024-05-12 10:34:35 +0530

    Create 394. Decode String.py

---
+++ python_codes/394. Decode String.py
@@ -0,0 +1,19 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        current_num = 0
+        current_string = ''
+        
+        for char in s:
+            if char.isdigit():
+                current_num = current_num * 10 + int(char)
+            elif char == '[':
+                stack.append((current_string, current_num))
+                current_string = ''
+                current_num = 0
+            elif char == ']':
+                prev_string, num = stack.pop()
+                current_string = prev_string + num * current_string
+            else:
+                current_string += char
+        return current_string
