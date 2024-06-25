3947f45a28814e2c96be55cd6416c5018f8a9e58
MARPU SAI MAHESH <117577374+saimahesh19@users.noreply.github.com>
2024-06-25 18:53:22 +0530

    Create 2. Add Two Numbers.py

---
+++ python_codes/2. Add Two Numbers.py
@@ -0,0 +1,22 @@
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        carry = 0
+        dummy_head = ListNode(0)
+        current = dummy_head
+        
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            total = val1 + val2 + carry
+            carry = total // 10 
+            new_val = total % 10  
+            current.next = ListNode(new_val)
+            current = current.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
+        return dummy_head.next
+
+            
+        
