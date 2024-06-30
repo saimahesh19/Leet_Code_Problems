87562b830adaf09b33aa9610705c92839dfe541f
MARPU SAI MAHESH <117577374+saimahesh19@users.noreply.github.com>
2024-06-30 18:53:48 +0530

    Create 486. Predict the Winner.py

---
+++ python_codes/486. Predict the Winner.py
@@ -0,0 +1,15 @@
+class Solution:
+    
+    def predictTheWinner(self, nums: List[int]) -> bool:
+
+        def turn (l, r, player):
+            
+            if l > r:
+                return 0
+            
+            if player:
+                return max(nums[l]+turn(l+1,r,not player), nums[r]+ turn(l, r-1, not player))
+            else:
+                return min(-nums[l]+turn(l+1,r,not player), -nums[r]+ turn(l, r-1, not player))
+
+        return turn(0, len(nums)-1, True) >= 0
