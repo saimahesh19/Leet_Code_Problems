378ef9787c792c3ea6275c5916ccf2e8fb040641
MARPU SAI MAHESH <117577374+saimahesh19@users.noreply.github.com>
2024-07-15 18:55:08 +0530

    Create 46. Permutations.py

---
+++ python_codes/46. Permutations.py
@@ -0,0 +1,15 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(current):
+            if len(current) == len(nums):
+                result.append(current[:])  
+                return
+            for num in nums:
+                if num not in current:
+                    current.append(num)
+                    backtrack(current)
+                    current.pop()  
+        result = []
+        backtrack([])
+        return result
+
